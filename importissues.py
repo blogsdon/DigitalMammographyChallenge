ffe59a9badbeef3b9c3fd54a5bf98b04f6a7d9a8
Benjamin Logsdon <aeberhart@gmail.com>
2016-11-11 09:56:42 -0800

    change repo to ampadknowledgeportal and default assignee to me

--- importissues.py
+++ importissues.py
@@ -23,30 +23,30 @@ def log(s):
 def main(argv=None):
     config = ConfigParser.ConfigParser()
     config.read(argv[1])
-    
-    
+
+
     f = open(argv[2], 'r')
     lastidString = f.read()
     lastid = int(lastidString)
     f.close()
-     
+
     log("last thread ID processed: "+str(lastid))
-     
+
     syn = synapseclient.Synapse()
-     
+
     syn.login(config.get('synapse', 'username'), apiKey=config.get('synapse', 'apiKey'))
-     
+
     gh = github3.login(token=config.get('github', 'token'))
-     
-    repository = gh.repository('Sage-Bionetworks', 'DigitalMammographyChallenge')
-    
+
+    repository = gh.repository('Sage-Bionetworks', 'ampadknowledgeportal')
+
     projectId=argv[3]
     forumId=argv[4]
     offset=0
     limit=20
-    
+
     maxNewId=lastid
-     
+
     totalNumberOfResults = sys.maxint
     while offset<totalNumberOfResults:
         threads=syn.restGET("/forum/"+str(forumId)+"/threads?limit="+str(limit)+"&offset="+str(offset)+"&filter=EXCLUDE_DELETED&sort=PINNED_AND_LAST_ACTIVITY&ascending=true")
@@ -55,28 +55,27 @@ def main(argv=None):
             threadid=int(thread.get('id'))
             if threadid<=lastid:
                 continue
-            
+
             if threadid>maxNewId:
                 maxNewId=threadid
-             
-            repository.create_issue(title=thread.get('title'), assignee="thomasyu888", body='https://www.synapse.org/#!Synapse:'+projectId+'/discussion/threadId='+str(thread.get('id')), labels=['discussion forum'])
-             
+
+            repository.create_issue(title=thread.get('title'), assignee="blogsdon", body='https://www.synapse.org/#!Synapse:'+projectId+'/discussion/threadId='+str(thread.get('id')), labels=['discussion forum'])
+
             log("Created issue for "+thread.get('id')+" "+thread.get('title'))
-            
+
         #now get the next batch
         offset = offset + limit
- 
+
     if maxNewId>lastid:
         f = open(argv[2], 'w')
         f.write(str(maxNewId))
         f.write('\n')
         f.close()
-         
+
     log("last thread ID processed: "+str(maxNewId))
-    
+
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
-    
